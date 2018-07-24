d37ad8462c17b50dcbfdf8502a86ff431c6f7096
Murilo Rodrigues <murilosisnando2003@hotmail.com>
2018-07-24 00:06:58 -0300

    push com erro auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,29 @@
-# app/__init__.py
-
 # third-party imports
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
-
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 # local imports
 from config import app_config
-from flask_migrate import Migrate
 
-
-# db variable initialization
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app(config_name):
-    
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-    db.init_app(app)
 
+    Bootstrap(app)
+    db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
-
     migrate = Migrate(app, db)
 
     from app import models
-
 
     from .admin import admin as admin_blueprint
     app.register_blueprint(admin_blueprint, url_prefix='/admin')
@@ -40,6 +33,5 @@ def create_app(config_name):
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
-
 
     return app
