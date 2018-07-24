c311ec50536a83216e91802f5bbb4e277e9478e9
Murilo Rodrigues <murilosisnando2003@hotmail.com>
2018-07-23 22:12:32 -0300

    push com erro auth

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 # local imports
 from config import app_config
 from flask_login import LoginManager
 from flask_migrate import Migrate
+
 
 # db variable initialization
 db = SQLAlchemy()
@@ -26,6 +28,15 @@ def create_app(config_name):
     migrate = Migrate(app, db)
 
     from app import models
+
+    from.admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    from.auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from.home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
 
 
     return app
