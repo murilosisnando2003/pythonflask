ac3197c5e4a9ca3fc5ae39926088547e7e19d42b
Murilo Rodrigues <murilosisnando2003@hotmail.com>
2018-07-24 21:07:56 -0300

    deploy

--- app/home/views.py
+++ app/home/views.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # app/home/views.py
 
-from flask import render_template
-from flask_login import login_required
+from flask import abort, render_template
+from flask_login import current_user, login_required
 
 from . import home
 
@@ -13,10 +13,12 @@ def homepage():
     """
     return render_template('home/index.html', title="Welcome")
 
-@home.route('/dashboard')
+# add admin dashboard view
+@home.route('/admin/dashboard')
 @login_required
-def dashboard():
-    """
-    Render the dashboard template on the /dashboard route
-    """
-    return render_template('home/dashboard.html', title="Dashboard")
+def admin_dashboard():
+    # prevent non-admins from accessing the page
+    if not current_user.is_admin:
+        abort(403)
+
+    return render_template('home/admin_dashboard.html', title="Dashboard")
