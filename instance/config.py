00116cb63c3b482a5eef160a27849f13504c858f
Murilo Rodrigues <murilosisnando2003@hotmail.com>
2018-07-25 21:16:25 -0300

    mensagem

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,4 @@
 # instance/config.py
 
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'mysql://dt_admin:dt2016@localhost/dreamteam_db'
+SQLALCHEMY_DATABASE_URI='mysql+pymysql://flask:Murilao23@flasktest.coppgkteunkn.us-east-1.rds.amazonaws.com:3306/flaskdb'
